380efdaf89b1b2c33a6b111c906ff34f722e646f
ilyastabennhas <109686507+ilyastabennhas@users.noreply.github.com>
2022-08-01 14:37:02 +0200

    Updates using the command line ..update 02

--- app/src/main/java/com/git_kt_project_01/MainActivity.kt
+++ app/src/main/java/com/git_kt_project_01/MainActivity.kt
@@ -2,10 +2,14 @@ package com.git_kt_project_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this,"Updates using the command line",Toast.LENGTH_LONG)
     }
 }
